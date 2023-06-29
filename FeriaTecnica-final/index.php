a4556518626a83acfcdb89233c54e96632d1dc05
Pinguno123 <douglasanchez.7b@gmail.com>
2023-06-29 14:29:06 -0600

    feriatecnica

---
+++ FeriaTecnica-final/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="recursos/css/main.css">
+    <title>Feria tecnica</title>
+</head>
+<body>
+    <header>
+        <?php include("navbar.php"); ?>
+        <div class="cabecera">
+            <h1 class="titulo">
+                Chemical Learning
+            </h1>
+            <p class="descripcion">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae hic soluta odio laboriosam nihil inventore incidunt, maiores praesentium reiciendis veniam!</p>
+        </div>
+    </header>
+    <main class="contenedor">
+        <section class="primerafila">
+            <div class="texto">
+                <h3>Juegos de Química</h3>
+            </div>
+            <div class="imagenes">
+                <a href=""><img src="" alt="Juego 1"></a>
+                <a href=""><img src="" alt="Juego 2"></a>
+                <a href=""><img src="" alt="Juego 3"></a>
+            </div>
+        </section>        
+        <section class="segundafila">
+            
+        </section>
+    </main>
+</body>
+</html>
---
+++ FeriaTecnica-final/navbar.php
@@ -0,0 +1,12 @@
+<?php $url_base = "Hola"?>
+<nav>
+    <h1>Chemical Learning</h1>
+    <div class="links">
+        <a href="">Juego 1</a>
+        <a href="">Juego 2</a>
+        <a href="">Juego 3</a>
+        <a href="">Juego 4</a>
+        <a href="">Juego 5</a>
+        <a href="">Juego 6</a>
+    </div>
+</nav>
